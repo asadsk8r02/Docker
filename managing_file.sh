a5b7f91fbe28b13e97bf360e1ce6affee179047d
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2024-09-18 17:05:00 -0500

    Update managing_file.sh

--- managing_file.sh
+++ managing_file.sh
@@ -3,12 +3,12 @@ COPY <source_URL> <destination>
 # NOTE: File from parent directory cannot be copied to the destination or app/ to preserve the context and isolation.
 
 # Donwload files instead of copying them.
-RUN curl <file-URL> -o <destination>
+RUN curl <file-URL> -o <destination_directory>
 # Unzip it
-RUN unzip <destination>/<filename>.zip
+RUN unzip <destination_directory>/<filename>.zip
 # Delete the original zip file
-RUN rm <copy_directory>/<filename>.zip
+RUN rm <destination_directory>/<filename>.zip
 
 # or
 # Preferred way to do all three at once and recommended to save image space. [Donwload, Unpack and remove files in single instruction.]
-RUN curl <file-URL> -o <destination>/<filename>.zip \ && unzip <destination>/<filename>.zip -d <unzipped_directory> \ && rm <copy_directory>/<filename>.zip
+RUN curl <file-URL> -o <destination>/<filename>.zip \ && unzip <destination>/<filename>.zip -d <unzipped_directory> \ && rm <destination>/<filename>.zip
