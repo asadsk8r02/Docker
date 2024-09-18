45c179469589d4167bde47711d39e2884df0e79e
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2024-09-18 02:13:55 -0500

    Update distribution.sh

--- distribution.sh
+++ distribution.sh
@@ -1,7 +1,11 @@
 # Distributing docker file - renaming
 docker tag <old_name> <private_docker_repository_URL>/<new_name>
-docker tag asad:v1 example.repository.com/asadkhan:v1
+# docker tag asad:v1 example.repository.com/asadkhan:v1
 
 docker push <private_docker_repository_URL>/<image_name>
 # or
 docker image push <private_docker_repository_URL>/<image_name>
+
+# Save image as a file
+docker save -o <file_name>.tar <image_name>
+docker save -o asad_file.tar asad:v2
