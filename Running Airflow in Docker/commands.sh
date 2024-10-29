ff8015a46df789d4a19a922d852a15ea9b6359b4
Asadullah Khan <asadkhanworkmail.@gmail.com>
2024-10-29 10:40:10 -0500

    Update commands.sh

--- Running Airflow in Docker/commands.sh
+++ Running Airflow in Docker/commands.sh
@@ -1,14 +1,19 @@
+# Create directory and navigate
 mkdir airfflow_docker
 cd airflow_docker
 
+# Chekc docker version
 docker --version
 docker-compose --version
 
+# Donwload docker-compose.yml file and create directories for the airflow.
 curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.10.2/docker-compose.yaml'
 mkdir -p ./dags ./logs ./plugins ./config
 
+# Initialize the database
 docker compose up airflow-init
 
+# Docker compose
 docker compose up -d
 
 # Navigate te http://localhost:8080
